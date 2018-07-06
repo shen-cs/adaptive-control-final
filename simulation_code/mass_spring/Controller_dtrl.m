a916b1e17d432388fb4b67234a50d8aabb279020
stationaryfront <stationaryfront211@gmail.com>
2018-07-06 17:50:02 +0800

    report finished

--- simulation_code/mass_spring/Controller_dtrl.m
+++ simulation_code/mass_spring/Controller_dtrl.m
@@ -4,8 +4,10 @@ classdef Controller_dtrl < handle
     
     properties
         S
+        S_temp
         Q
         R
+        x_int
     end
     
     methods
@@ -14,9 +16,10 @@ classdef Controller_dtrl < handle
             %   Detailed explanation goes here
             %   n is the dimension of state vector 
             obj.S = S;
+            obj.S_temp = S;
             obj.Q = Q;
             obj.R = R;
-            
+            obj.x_int;
         end
         
         function set.S(obj, value)
@@ -27,30 +30,37 @@ classdef Controller_dtrl < handle
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             n = size(obj.Q, 1);
-            alpha = 0.1;
-            gamma = 1;
+            alpha = 0.5;
+            gamma = 0.99;
             x = z(1:n);
             u = z(n+1:end);
             % u* = argmin Q(x_next, u);
             % Q_min = Q(x_next, u*)
-            u_min = obj.policy_improvement(x_next);
+            u_min = obj.policy_improvement(x_next, obj.x_int);
             z_min = [x_next; u_min];
             q_min = z_min'*obj.S*z_min;
-            % delta = x'*obj.Q*x + u'*obj.R*u + gamma*q_min - z'*obj.S*z;
+            % delta = x'*obj.Q*x + u'*obj.R*u + gamma*q_min - z'*obj.S*z; % q-learning
             z_next = [x_next; u_min];
-            delta = x'*obj.Q*x + u'*obj.R*u + gamma*z_next'*obj.S*z_next - z'*obj.S*z;
-            disp(delta);
+            delta = x'*obj.Q*x + u'*obj.R*u + gamma*z_next'*obj.S*z_next - z'*obj.S*z;% sarsa
+            %disp(delta);
             dS = z * z';
-            obj.S = obj.S - alpha * delta * dS;
-            S = obj.S;
+            S_prev = obj.S_temp;
+            obj.S_temp = obj.S_temp + alpha * delta * dS;
+            if abs(det(obj.S_temp-S_prev)) < 0.0001
+                obj.S = obj.S_temp;
+            end
+            S = obj.S_temp;
         end
         
-        function u = policy_improvement(obj, x)
+        function u = policy_improvement(obj, x, x_int)
             % suppose x is n-vector, u is m-vector, n + m = l
             n = size(obj.Q, 1);
             Suu = obj.S(n+1:end, n+1:end); % m * m
             Sux = obj.S(n+1:end, 1:n); % m * n
-            u = -inv(Suu)*Sux*x;
+            K = Suu\Sux;
+            % u = -K*x;
+            u = -[K 0.8]*[x;x_int] + rand(1, 1) * 5;
+            obj.x_int = x_int;
         end
         
         function u = learn(obj, z, z_next)
--- simulation_code/mass_spring/reg_massSpring.m
+++ simulation_code/mass_spring/reg_massSpring.m
@@ -4,40 +4,68 @@ clc;
 
 m = 10;
 k = 9;
-b = 1;
+b = 4;
 A = [0 1; -k/m -b/m];
 B = [0; 1/m];
 
 sys = ss(A, B, eye(2), zeros(2, 1));
 sysd = c2d(sys, .001);
 sys_ss = ss(sysd);
-
+A = sys_ss.A;
+B = sys_ss.B;
 Q = [10 0; 0 1];
 R = 0.001;
 
 [P, E, G] = dare(sys_ss.A, sys_ss.B, Q, R);
-sim_time = 60;
+sim_time = 30;
 dt = .001;
 interval = 0.1 / dt;
 tspan = 0:dt:sim_time;
 
-x0 = [2; 0];
+x0 = [3; -4];
 x = x0;
-x_target = [1; 0];
-S = [A'*P*A+Q A'*P*B; B'*P*A B'*P*B+R];
+x_target = [0; 0];
+t_cost = 0:interval*dt:sim_time;
+x_history = zeros(length(t_cost), 1);
+p_history = zeros(2, 2, length(t_cost));
+%S_true = [A'*P*A+Q A'*P*B; B'*P*A B'*P*B+R];
+S = ones(3) + rand(3, 3);
 controller_rl = Controller_dtrl(S, Q, R);
-
+x_int = 0;
 for i=0:length(tspan)
     x_error = x - x_target;
     if mod(i, interval) == 0
         drawModel(x);
+        x_history(i ./ interval+1) = x(1);
+        Sxx_history = controller_rl.S(1:2, 1:2);
+        [V, D] = eig(Sxx_history-Q);
+        p_history(:, :, i ./ interval + 1) = D;
     end
-    % u = controller_rl.policy_improvement(x_error);
-    u = -30*(x_error(1));
-    % Suu = S(3:end, 3:end);
-    % Sux = S(3:end, 1:2);
-    % u = -inv(Suu)*Sux*(x-x_target);
-    dx = massSpring_ct(x, m, k, b, u);
+    u = controller_rl.policy_improvement(x_error, x_int);
+    % u = -[G 20]*[x_error; x_int];
+    % u = -[80 50 20]*[x_error; x_int]; % PID control
+%     Suu = S(3:end, 3:end);
+%     Sux = S(3:end, 1:2);
+%     K = Suu\Sux;
+%     u = -K*(x-x_target);
+    dx = massSpring_ct(x, m, k, b, u) + rand(2, 1)*.1;
+    x_error_prev = x_error;
     x = x + dx * dt;
+    x_error = x - x_target;
+    x_int = x_int + x_error(1)*dt;
+    controller_rl.policy_evaluation([x_error_prev; u], x_error);
 end
 disp(x);
+%disp(S(1, 1)); % optimal S(1, 1) = 5.33e03
+disp(P);
+disp(p_history(:, :, end));
+figure;
+ax1 = subplot(2, 1, 1);
+plot(ax1, t_cost, x_history);
+title('x');
+ax2 = subplot(2, 1, 2);
+plot(ax2, t_cost, squeeze(p_history(1, 1, :)));
+hold on;
+plot(ax2, t_cost, squeeze(p_history(2, 2, :)));
+legend('P11', 'P22');
+title('P');
